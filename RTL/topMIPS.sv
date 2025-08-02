2dc88b8a7f9c2ca2109fb6b798edd06010fc79ed
Nick A. <namore7@gmail.com>
2025-08-02 12:14:19 -0400

    Create topMIPS.sv
    Setup of repo

---
+++ RTL/topMIPS.sv
@@ -0,0 +1,18 @@
+/*
+* @file topMIPS.sv 
+* @brief Top level of MIPS architecture implementation
+* @author Nicholas Amore namore7@gmail.com
+* @date Creater 8/1/2025
+*/
+
+module topMIPS (
+  input clk_i,
+  input rst_ni;
+
+);
+
+logic [23:0] addr_bus;
+logic [15:0] data_bus;
+
+
+endmodule
